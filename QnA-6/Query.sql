fbb8da89574d93db97df9928ec5fcfa5a0c1ad38
Mousam Dey <76545841+Mousam98@users.noreply.github.com>
2024-03-09 01:12:46 +0530

    Create Query.sql

---
+++ QnA-6/Query.sql
@@ -0,0 +1,28 @@
+/*
+	6. Which do you think is more helpful - hints or videos? Explain your reasoning.
+*/
+with temp as (
+	select user_id, action,
+  	row_number() over (partition by user_id order by timestamp) as action_rank
+  from interactions
+),
+
+user_metrics as(
+  select distinct u1.user_id,
+  case
+      when u1.user_id = u2.user_id and 
+      (u1.action = 'correct' and u2.action = 'video') and
+      (u1.action_rank = u2.action_rank+1) then "Yes"
+      else	"No" end
+  as video_clip_helped,
+  case
+      when u1.user_id = u2.user_id and
+      (u1.action = 'correct' and u2.action = 'hint') and
+      (u1.action_rank = u2.action_rank+1) then 'Yes'
+      else	'No' end
+  as hints_helped
+  from temp u1, temp u2
+)
+
+select * from user_metrics
+where video_clip_helped = 'Yes' or hints_helped = 'Yes';

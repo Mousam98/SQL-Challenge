b5954f93c0e652e24b83f4f2d99397eeb2b3177d
Mousam Dey <76545841+Mousam98@users.noreply.github.com>
2024-03-09 01:06:08 +0530

    Create query.sql

---
+++ QnA-1/query.sql
@@ -0,0 +1,25 @@
+/*
+	IMPORTANT INSTRUCTIONS:
+	1) Before you do anything, click on "Fork" (top left).
+    	This will create a copy of the assessment, tied to a new URL.
+        It is recommended for you to save this URL while you are working.
+    
+    2) Fill in your name into the "Fiddle Title" (left).
+    
+    3) There are a list of tasks for you below, based on the schema built on the left.
+    	It consists of two tables: users and interactions.
+        You can write SQL below and click on "Run" (top left) to run the queries.
+        You can also include queries that don't directly answer the question, if you think that will be helpful.
+        You can also leave comments to explain or elaborate on your answer.
+    
+    4) You can save your progress by clicking on "Update" (top left).
+    
+	5) Once you are finished with the task, click on "Update" one last time.
+    	Then copy the URL, and include the URL in your job application.
+*/
+
+/*
+	There might be a popup that you cannot close. Run any query and you should be able to close it.
+	Example query:
+*/
+SELECT * FROM users;
